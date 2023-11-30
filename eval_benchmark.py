50bf5f22df0a431c3650b90edee4d6337a527d8d
Helw150 <wbh230@nyu.edu>
2023-11-30 15:01:39 -0500

    Fix

--- eval_benchmark.py
+++ eval_benchmark.py
@@ -3,22 +3,8 @@ import os
 import torch
 import numpy as np
 import pandas as pd
-from categories import subcategories, categories
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 import time
-
-choices = ["A", "B", "C", "D"]
-
-
-def gen_prompt(train_df, subject, k=-1):
-    prompt = "The following are multiple choice questions (with answers) about {}.\n\n".format(
-        format_subject(subject)
-    )
-    if k == -1:
-        k = train_df.shape[0]
-    for i in range(k):
-        prompt += format_example(train_df, i)
-    return prompt
 
 
 @torch.no_grad()
