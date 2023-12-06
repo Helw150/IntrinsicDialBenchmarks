1c36cfbe32bfc0ac0d82fde68f9db5573bf4a4d6
helw150 <me@williamheld.com>
2023-12-06 05:22:09 +0000

    Git Profile

--- setup_env.sh
+++ setup_env.sh
@@ -1,2 +1,4 @@
 pip install accelerate transformers
 pip install tiktoken einops transformers_stream_generator==0.0.4
+git config --global user.email "me@williamheld.com"
+git config --global user.name "helw150"
