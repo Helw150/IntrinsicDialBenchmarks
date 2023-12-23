cd7ef9a061d897cde538934265019d9d2a9f7877
Helw150 <wbh230@nyu.edu>
2023-12-23 14:42:30 -0500

    Eval Update

--- syntax/create_negatives.py
+++ syntax/create_negatives.py
@@ -3,8 +3,7 @@ import random
 
 import pandas as pd
 from datasets import load_dataset
-from src.Dialects import (DialectFromFeatureList, DialectFromVector,
-                          IndianDialect)
+from src.Dialects import DialectFromFeatureList, DialectFromVector, IndianDialect
 from tqdm import tqdm
 
 random.seed(42)
@@ -12,8 +11,19 @@ random.seed(42)
 ie = IndianDialect()
 rev_ie = DialectFromVector(vector=(ie.vector + 1) * (ie.vector == 0))
 features = [key for key, value in rev_ie.morphosyntax_transforms.items()]
+
 transformations = [
-    DialectFromFeatureList(feature_list=[feature]) for feature in features
+    DialectFromFeatureList(feature_list=[feature])
+    for feature in features
+    if feature
+    not in [
+        "a_ing",
+        "indefinite_for_zero",
+        "adj_postfix",
+        "existential_it",
+        "you_ye",
+        "do_tense_marker",
+    ]
 ]
 
 df = load_dataset("WillHeld/demszky_pairs")["train"].to_pandas()
@@ -27,12 +37,19 @@ df = df.groupby("phrase_ID").sample(1)
 gen_e = df["sentence_x"].array.tolist()
 ind_e = df["sentence_y"].array.tolist()
 invents = [
-    [transform.transform(ex) for transform in transformations] for ex in tqdm(gen_e)
+    [
+        (transform.transform(ex), transform.morphosyntax_transforms)
+        for transform in transformations
+    ]
+    for ex in tqdm(gen_e)
 ]
+
 invents = [
-    random.sample([ex for ex in invent if ex != gen_e[i]], 1)[0]
+    [(ex, name) for ex, name in invent if ex != gen_e[i]]
     for i, invent in enumerate(invents)
 ]
+
+invents = [random.sample(invent, 1)[0][0] for invent in invents]
 
 gen_e_jsons = []
 for ex in gen_e:
@@ -42,8 +59,8 @@ for ex in gen_e:
         prompt += "A: Acceptable\nB: Unacceptable"
     elif letter == "B":
         prompt += "A: Unacceptable\nB: Acceptable"
-    prompt += "\n\n Answer: "
-    gen_e_jsons.append({"prompt": prompt, "correct_answer": letter})
+        prompt += "\n\n Answer: "
+        gen_e_jsons.append({"prompt": prompt, "correct_answer": letter})
 
 ind_e_jsons = []
 for ex in ind_e:
@@ -53,8 +70,8 @@ for ex in ind_e:
         prompt += "A: Acceptable\nB: Unacceptable"
     elif letter == "B":
         prompt += "A: Unacceptable\nB: Acceptable"
-    prompt += "\n\n Answer: "
-    ind_e_jsons.append({"prompt": prompt, "correct_answer": letter})
+        prompt += "\n\n Answer: "
+        ind_e_jsons.append({"prompt": prompt, "correct_answer": letter})
 
 invent_jsons = []
 for ex in invents:
@@ -64,8 +81,8 @@ for ex in invents:
         prompt += "A: Acceptable\nB: Unacceptable"
     elif letter == "B":
         prompt += "A: Unacceptable\nB: Acceptable"
-    prompt += "\n\n Answer: "
-    invent_jsons.append({"prompt": prompt, "correct_answer": letter})
+        prompt += "\n\n Answer: "
+        invent_jsons.append({"prompt": prompt, "correct_answer": letter})
 
 with open("demszky_indian_english_syntax_quiz.json", "w") as json_file:
     for entry in ind_e_jsons:
